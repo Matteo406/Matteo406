31c40fde430b37cc089f350a8d9945c29a25733f
Tim Paris <tim.paris@bossinfo.ch>
2024-05-15 09:58:16 +0200

    update keylogs

--- keylog.py
+++ keylog.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from dotenv import load_dotenv
 import git
 import os
+import schedule
+import time
 
 # Load the .env file
 load_dotenv()
@@ -15,42 +17,47 @@ repo_dir = os.getenv('PATH_TO_REPO')
 key_count = 0
 click_count = 0
 
+# Initialize last counts
+last_key_count = 0
+last_click_count = 0
+
 def on_press(key):
     global key_count
     key_count += 1
-    save_counts()
 
 def on_click(x, y, button, pressed):
     global click_count
     if pressed:
         click_count += 1
-        save_counts()
 
 def save_counts():
-    # Check if repo_dir is set
-    if repo_dir is None:
-        print('Please set the repo_dir variable in the .env file')
-        return
+    global last_key_count, last_click_count
 
-    # Check if log_file var is set
-    if log_file is None:
-        print('Please set the log_file variable in the .env file')
-        return
+    # Only commit and push if the counts have changed
+    if key_count != last_key_count or click_count != last_click_count:
+        with open(log_file, 'w') as f:
+            f.write(f'<!--START_SECTION:activity-->\n\n')
+            f.write(f'From: {datetime.now().strftime("%d %B %Y")} - To: {datetime.now().strftime("%d %B %Y")}\n\n')
+            f.write(f'Total Keystrokes: {key_count}\n')
+            f.write(f'Total Mouse Clicks: {click_count}\n')
+            f.write(f'\n<!--END_SECTION:activity-->\n')
 
-    with open(log_file, 'w') as f:
-        f.write(f'<!--START_SECTION:activity-->\n\n')
-        f.write(f'From: {datetime.now().strftime("%d %B %Y")} - To: {datetime.now().strftime("%d %B %Y")}\n\n')
-        f.write(f'Total Keystrokes: {key_count}\n')
-        f.write(f'Total Mouse Clicks: {click_count}\n')
-        f.write(f'\n<!--END_SECTION:activity-->\n')
+        # Commit and push
+        repo = git.Repo(repo_dir)
+        repo.git.add(log_file)
+        repo.git.commit('-m', 'update log file')
+        repo.git.push()
 
-    # Commit and push
-    repo = git.Repo(repo_dir)
-    repo.git.add(log_file)
-    repo.git.commit('-m', 'update log file')
-    repo.git.push()
+        # Update last counts
+        last_key_count = key_count
+        last_click_count = click_count
+
+# Schedule the save_counts function to be called every 5 minutes
+schedule.every(5).minutes.do(save_counts)
 
 # Start the listeners
 with keyboard.Listener(on_press=on_press) as k_listener, mouse.Listener(on_click=on_click) as m_listener:
-    k_listener.join()
-    m_listener.join()
+    print('Listening...')
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
