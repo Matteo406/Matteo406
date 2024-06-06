c97203e33fc317e4e16c4ef4e61e97cff9f8841d
Matteo406 <92585968+Matteo406@users.noreply.github.com>
2024-06-06 15:01:01 +0200

    update

--- keylog.py
+++ keylog.py
@@ -13,6 +13,7 @@ import re
 # Load the .env file
 load_dotenv()
 
+
 # Define the log file and the repo
 log_file = os.getenv('PATH_TO_LOGFILE')
 repo_dir = os.getenv('PATH_TO_REPO')
@@ -101,11 +102,11 @@ def commit_and_push():
     # Log script finish
 
 # Schedule the save_counts function to be called every 5 minutes
-schedule.every(5).minutes.do(save_counts)
+schedule.every(1).minutes.do(save_counts)
 
 # Schedule the commit_and_push function to be called every hour
-schedule.every(1).hours.do(commit_and_push)
-# schedule.every(1).minutes.do(commit_and_push)
+# schedule.every(1).hours.do(commit_and_push)
+schedule.every(1).minutes.do(commit_and_push)
 
 # Start the listeners
 with keyboard.Listener(on_press=on_press) as k_listener, mouse.Listener(on_click=on_click) as m_listener:
