987aa8625aef83dac09b757052d128cb8e3e127f
Tim Paris <tim.paris@bossinfo.ch>
2024-06-10 16:29:38 +0200

    update

--- updateREADME.py
+++ updateREADME.py
@@ -19,7 +19,7 @@ with open('README.md', 'r+') as readme:
          continue
       percentage = (value / totalPressEvents) * 100
       bars = '█' * int(percentage / 5) + '░' * (20 - int(percentage / 5))
-      stats += f'{key:<20} {value} {bars} {percentage:.2f} % \n'
+      stats += f'{key:<20} {value:<10} {bars} {percentage:.2f} % \n'
     new_content = re.sub('<!--START_SECTION:activity-->.*<!--END_SECTION:activity-->', f'<!--START_SECTION:activity-->\n```txt\n{stats}\n```\n<!--END_SECTION:activity-->', content, flags=re.DOTALL)
     readme.seek(0)
     readme.write(new_content)
